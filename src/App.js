fdb215e1b1d0de801091666fdbb5679da15a966c
Sushant Thapa <sushant.chhetri@>
2020-09-20 09:23:04 +1000

    router setup done

--- src/App.js
+++ src/App.js
@@ -4,15 +4,23 @@ import Error from './pages/Error';
 import Home from './pages/Home';
 import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
-
+import { Route, Link, Switch } from "react-router-dom";
 function App() {
   return (
     <>
-      hello from app
-      <Home />
+      <Switch>
+        <Route exact path="/" >
+          <Home />
+        </Route>
+        <Route exact path="/rooms" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
+      {/* hello from app
+      < Home />
       <Rooms />
       <SingleRoom />
-      <Error />
+      <Error /> */}
     </>
   );
 }
